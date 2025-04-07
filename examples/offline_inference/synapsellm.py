ef1a1a35e3328ddfa603413329e9fb6a91607726
Zhenzhong1 <zhenzhong.xu@intel.com>
2025-04-07 07:54:44 +0000

    inference pass

--- examples/offline_inference/synapsellm.py
+++ examples/offline_inference/synapsellm.py
@@ -27,6 +27,7 @@ os.environ['VLLM_SYNAPSELLM_NUM_THREADS'] = "32"
 # Sample prompts.
 prompts = [
     '3.9和3.11哪个数更大？',
+    #'你怎么回事啊？',
     #"Hello, my name is,Hello, my name is,Hello, my name is,Hello, my name is,Hello, my name is,Hello, my name is,Hello, my name is,Hello, my name is",
     # "The president of the United States is",
     # "The capital of France is",
@@ -84,7 +85,7 @@ llm = LLM(
     # The max_model_len and block_size arguments are required to be same as
     # max sequence length when using SynapseLLM backend (no PagedAttention).
     max_model_len=1024,
-    block_size=4,
+    block_size=2,
     # The device can be automatically detected when setting `VLLM_TARGET_DEVICE=synapsellm`
     # before vllm  installing process.
     # The device argument can be either unspecified for automated detection,
